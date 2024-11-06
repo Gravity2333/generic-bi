8c1d24a79ec3e62c75537c6b388b3fb96607f9f2
liuze <liuze@machloop.com>
2024-11-06 19:33:50 +0800

    feat: card

--- packages/bi-manager-web/src/pages/Welcome/index.tsx
+++ packages/bi-manager-web/src/pages/Welcome/index.tsx
@@ -1,8 +1,8 @@
 import { checkDabaseConnect } from '@/services/database';
 import { DisconnectOutlined, SmileOutlined } from '@ant-design/icons';
-import { Alert, Button, Result } from 'antd';
+import { Alert, Button, Card, Result } from 'antd';
 import { useEffect, useState } from 'react';
-import {history} from 'umi'
+import { history } from 'umi';
 
 export default function Welcome() {
   const [connect, setConnect] = useState<boolean>(true);
@@ -21,15 +21,39 @@ export default function Welcome() {
           message="数据库连接失败，请检查配置！"
           style={{ margin: '20px', borderRadius: '10px' }}
           action={
-            <Button size="small" type="link" onClick={()=>{
-              history.push('/configuration/database')
-            }}>
+            <Button
+              size="small"
+              type="link"
+              onClick={() => {
+                history.push('/configuration/database');
+              }}
+            >
               去配置
             </Button>
           }
         />
       ) : null}
       <Result icon={<SmileOutlined />} title={`欢迎使用Generic-BI`} />
+      <div
+        style={{
+          display: 'flex',
+          flexFlow: 'row wrap',
+          justifyContent: 'start',
+          margin: 'auto',
+          width: '70%',
+        }}
+      >
+        <Card
+          size="small"
+          style={{ width: '25%', height: '200px', borderRadius: '10px', margin: '10px' }}
+          title={undefined}
+        >
+          ss
+        </Card>
+        <Card  style={{ width: '25%', height: '200px', borderRadius: '10px', margin: '10px' }}size="small" title={undefined}>
+          ss
+        </Card>
+      </div>
     </>
   );
 }
