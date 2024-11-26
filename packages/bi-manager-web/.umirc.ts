2cf62830d8af53020b51f890eb1eb363de20c996
liuze <liuze@machloop.com>
2024-11-26 14:06:02 +0800

    feat: 增加性分析

--- packages/bi-manager-web/.umirc.ts
+++ packages/bi-manager-web/.umirc.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'umi';
 import moment from 'moment';
 
 export const isDev = process.env.NODE_ENV === 'development';
+const { BundleAnalyzerPlugin } = require('umi-webpack-bundle-analyzer');
 
 export default defineConfig({
   nodeModulesTransform: {
@@ -35,5 +36,70 @@ export default defineConfig({
       .rule('mjs-rule')
       .test(/\.m?js$/)
       .resolve.set('fullySpecified', false);
+
+    if (process.env.ANALYZE === '1') {
+      config.plugin('umi-webpack-bundle-analyzer').use(
+        new BundleAnalyzerPlugin({
+          analyzerMode: 'server', // 启用分析模式
+          analyzerPort: 9765, // 设置端口号为 9765,
+          openAnalyzer: true, // 自动打开分析页面
+        }),
+      );
+    }
+
+    config.optimization.splitChunks({
+      chunks: 'all',
+      cacheGroups: {
+        vendor: {
+          name: 'vendor',
+          test: /[\\/]node_modules[\\/]/,
+          chunks: 'all',
+          priority: 10,
+          reuseExistingChunk: true,
+          enforce: true,
+        },
+        react: {
+          name: 'react',
+          test: /[\\/]node_modules[\\/](react|react-dom|react-router-dom|react-redux|redux)[\\/]/,
+          chunks: 'all',
+          // 要比node_modules高一点
+          priority: 20,
+          reuseExistingChunk: true,
+          enforce: true,
+        },
+        ecahrts: {
+          name: 'ecahrts',
+          test: /[\\/]node_modules[\\/]echarts/,
+          chunks: 'all',
+          // 要比node_modules高一点
+          priority: 20,
+          reuseExistingChunk: true,
+          enforce: true,
+        },
+        'ag-grid': {
+          name: 'ag-grid', // 分组名称
+          test: /[\\/]node_modules[\\/]ag-grid-community/, // 匹配工具库
+          chunks: 'all',
+          priority: 20,
+          maxSize: 1024*1024,
+          enforce: true,
+        },
+        'wangEditor': {
+          name: 'wangEditor', // 分组名称
+          test: /[\\/]node_modules[\\/]wangeditor/, // 匹配工具库
+          chunks: 'all',
+          priority: 20,
+          enforce: true,
+        },
+        utils: {
+          name: 'utils', // 分组名称
+          test: /[\\/]node_modules[\\/](lodash|moment|dayjs)/, // 匹配工具库
+          chunks: 'all',
+          priority: 10,
+          enforce: true,
+        },
+      },
+    });
+    config.optimization.set('emitOnErrors', false); // 替代 noEmitOnErrors
   },
 });
